f43274a01a52c0adb0f5066a84da04d33c4c0d91
Nick Cherry <nickcherryjiggz@gmail.com>
2023-10-31 14:50:49 -0400

    add .DS_Store to gitignore

--- src/scripts/content/index.ts
+++
@@ -1,8 +0,0 @@
-import { onMessage } from 'scripts/utils/chrome/onMessage';
-
-onMessage<string, string>((message, _sender, sendResponse) => {
-  console.log('Content script received message from popup:', message);
-  sendResponse(message.toUpperCase() + '!!!');
-
-  return false;
-});
